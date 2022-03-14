0d11c84ec8145235461bbde18c23d2af185fa9d2
liyucheng <liyucheng_2@qq.com>
2022-03-14 14:00:21 +0000

    add trans scripts

--- eval/trans.py
+++ eval/trans.py
@@ -1,4 +1,6 @@
 from lyc.utils import BaiduTranslator
+import pandas as pd
+import os
 
 trans_tasks = [
     ('generated_examples/GPT2.tsv', 'zh', 'en'),
@@ -6,6 +8,12 @@ trans_tasks = [
 ]
 
 if __name__ == '__main__':
+    translator = BaiduTranslator()
     for task in trans_tasks:
         file_path, from_lang, to_lang = task
-        
+        with open(file_path, encoding='utf-8') as f:
+            query = f.read()
+            responds = translator.make_request(query, from_lang, to_lang)
+            results = responds['trans_result']
+            df = pd.DataFrame(results)
+            df.to_csv(file_path+'.out', sep='\t', index=False)
